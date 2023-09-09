fb2ca0da86abf7d17054ecc5ad2f4c99c34cd779
hongduccodedao <contact.hongduc@gmail.com>
2023-09-09 21:03:01 +0700

    ✨ Login form

--- GUI/DangNhap.Designer.cs
+++ GUI/DangNhap.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DangNhap));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPasswordLogin = new System.Windows.Forms.TextBox();
+            this.txtUsernameLogin = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-3, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(498, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.btnLogin);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtPasswordLogin);
+            this.panel2.Controls.Add(this.txtUsernameLogin);
+            this.panel2.Location = new System.Drawing.Point(501, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(299, 400);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(172, 230);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(111, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tên đăng nhập";
+            // 
+            // txtPasswordLogin
+            // 
+            this.txtPasswordLogin.Location = new System.Drawing.Point(11, 204);
+            this.txtPasswordLogin.MaxLength = 20;
+            this.txtPasswordLogin.Name = "txtPasswordLogin";
+            this.txtPasswordLogin.Size = new System.Drawing.Size(272, 20);
+            this.txtPasswordLogin.TabIndex = 0;
+            this.txtPasswordLogin.UseSystemPasswordChar = true;
+            // 
+            // txtUsernameLogin
+            // 
+            this.txtUsernameLogin.Location = new System.Drawing.Point(11, 148);
+            this.txtUsernameLogin.MaxLength = 20;
+            this.txtUsernameLogin.Name = "txtUsernameLogin";
+            this.txtUsernameLogin.Size = new System.Drawing.Size(272, 20);
+            this.txtUsernameLogin.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(495, 399);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 399);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "DangNhap";
             this.Text = "DangNhap";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DangNhap_FormClosing);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.DangNhap_KeyPress);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtPasswordLogin;
+        private System.Windows.Forms.TextBox txtUsernameLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- GUI/DangNhap.cs
+++ GUI/DangNhap.cs
@@ -15,6 +15,58 @@ namespace GUI
         public DangNhap()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        public void loginEvent()
+        {
+            String username = txtUsernameLogin.Text;
+            String password = txtPasswordLogin.Text;
+
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (username == "admin" && password == "admin")
+            {
+                this.Hide();
+                Dashboard dashboard = new Dashboard();
+                dashboard.ShowDialog();
+                this.Show();
+            }
+            else if (username == "user" && password == "user")
+            {
+                this.Hide();
+                Order order = new Order();
+                order.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy tài khoản, vui lòng liên hệ quản lý để cung cấp tài khoản!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void DangNhap_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thực sự muốn thoát chương trình?", "Xác nhận thoát", MessageBoxButtons.OKCancel) != DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            loginEvent();
+        }
+
+        private void DangNhap_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //khi ấn enter thực hiện đăng nhập
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                loginEvent();
+            }
         }
     }
 }
