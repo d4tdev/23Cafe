b90e2af684e30e7ebe5ca4ee152d9db49b4e56a7
hongduccodedao <contact.hongduc@gmail.com>
2023-09-11 15:58:51 +0700

    ✏️ Update Database and view information detail product

--- GUI/Product/ProductManager.cs
+++ GUI/Product/ProductManager.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
 
 namespace GUI
 {
@@ -16,11 +18,19 @@ namespace GUI
         public ProductManager()
         {
             InitializeComponent();
+            loadData();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void loadData()
         {
-
+            String connectionString = "Data Source=HDCD-INSPIRON-3\\HDUC;Initial Catalog=QuanLy23Cafe;Integrated Security=True;Connect Timeout=30";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Food", connection);
+            DataTable dt = new DataTable();
+            connection.Open();
+            adapter.Fill(dt);
+            dataGridViewFood.DataSource = dt;
+            connection.Close();
         }
 
         private void btnAddProduct_Click(object sender, EventArgs e)
@@ -38,6 +48,14 @@ namespace GUI
         private void button1_Click(object sender, EventArgs e)
         {
             String search = txtSearchProduct.Text;
+        }
+
+        private void dataGridViewFood_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textCodeProduct.Text = dataGridViewFood.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textNameProduct.Text = dataGridViewFood.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textPriceProduct.Text = dataGridViewFood.Rows[e.RowIndex].Cells[4].Value.ToString();
+            textCategoryProduct.Text = dataGridViewFood.Rows[e.RowIndex].Cells[3].Value.ToString();
         }
     }
 }
