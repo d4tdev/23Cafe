7cc1fccc600b800e1a0d4868df8a6d2d4b5ebda2
hongduccodedao <contact.hongduc@gmail.com>
2023-09-14 17:54:48 +0700

    💄 UI Add Staff

--- GUI/Staff/AddStaff.cs
+++ GUI/Staff/AddStaff.cs
@@ -16,5 +16,24 @@ namespace GUI.Staff
         {
             InitializeComponent();
         }
+
+        private void btnCreateStaff_Click(object sender, EventArgs e)
+        {
+            String username = txtUsername.Text;
+            String password = txtPassword.Text;
+            String name = txtName.Text;
+            String phone = txtPhone.Text;
+            int salary = Convert.ToInt32(txtSalary.Text);
+
+            if (username == null || password == null || name == null || phone == null || salary == null)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Staff created successfully");
+                this.Close();
+            }   
+        }
     }
 }
