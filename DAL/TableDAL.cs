8ff71960e30cf0f3f5de38e99c43e75639fbea98
hongduccodedao <contact.hongduc@gmail.com>
2023-12-10 18:40:10 +0700

    Update

--- DAL/TableDAL.cs
+++ DAL/TableDAL.cs
@@ -3,10 +3,6 @@ using DTO;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace DAL
 {
